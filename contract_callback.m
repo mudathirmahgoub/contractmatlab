0923386852c0dd9fa754b3daff2e6d402c7c5b6c
mudathirmahgoub <mudathirmahgoub@gmail.com>
2017-10-17 09:54:01 -0500

    Added require blocks and ensure blocks

--- contract_callback.m
+++ contract_callback.m
@@ -42,8 +42,13 @@ set_param(block,'MaskDisplay',char(portStr));
  %% add or remove blocks
     blockModel = get_param(gcb, 'Parent');              
     ports = get_param(gcb,'PortHandles');
-    portConnectivity = get_param(gcb, 'PortConnectivity');          
+    portConnectivity = get_param(gcb, 'PortConnectivity'); 
+    
     for i = 1 : length(portConnectivity)
+        
+        requireBlock = -1;
+        ensureBlock = -1;
+        
         % if the port is not connected
         if portConnectivity(i).SrcBlock == -1
             % add a new block
@@ -54,6 +59,8 @@ set_param(block,'MaskDisplay',char(portStr));
                     blockHandle =  add_block('Kind/guarantee',strcat(blockModel,'/','guarantee'),'MakeNameUnique','on');
                 else 
                     blockHandle =  add_block('Kind/mode',strcat(blockModel,'/','mode'),'MakeNameUnique','on');
+                    requireBlock = add_block('Kind/require',strcat(blockModel,'/','require'),'MakeNameUnique','on');
+                    ensureBlock = add_block('Kind/ensure',strcat(blockModel,'/','ensure'),'MakeNameUnique','on');
                 end
             end  
            % move the new block closer to its port
@@ -69,16 +76,31 @@ set_param(block,'MaskDisplay',char(portStr));
            % connect the new block with its port
            blockPorts = get_param(blockHandle, 'PortConnectivity');
            [outputPortIndex , ~] = size(blockPorts);
-           add_line(blockModel, [blockPorts(outputPortIndex).Position; portConnectivity(i).Position ]); 
+           add_line(blockModel, [blockPorts(outputPortIndex).Position; portConnectivity(i).Position ]);            
            
            % add all assumptions to each mode port
            if i > assumePorts + guaranteePorts
-               % assume is the first inport in mode block
-               %add_line(blockModel, [portConnectivity(assumptionsPortIndex).Position; blockPorts(1).Position]); 
+               % get the mode ports
                modePorts = get_param(blockHandle, 'PortHandles');
+               % connect require and ensure blocks to the mode block
+               if requireBlock ~= -1
+                   % get the require ports
+                   requirePorts = get_param(requireBlock, 'PortHandles');
+                   % connect the require port with the mode block
+                    add_line(blockModel, requirePorts.Outport(1) ,modePorts.Inport(2), 'autorouting','on');
+               end
+               
+               if ensureBlock ~= -1
+                   % get the ensure ports
+                   ensurePorts = get_param(ensureBlock, 'PortHandles');
+                   % connect the ensure port with the mode block
+                    add_line(blockModel, ensurePorts.Outport(1) ,modePorts.Inport(3), 'autorouting','on');
+               end
+               
                 %check if the mode assume port is already connected
                 portLine = get_param(modePorts.Inport(1),'Line'); 
                 if portLine == -1   
+                    % connect the assumption port with the mode block
                     add_line(blockModel, ports.Outport(1) ,modePorts.Inport(1), 'autorouting','on');
                 end
                %register a callback function when the mode inport
@@ -93,6 +115,10 @@ set_param(block,'MaskDisplay',char(portStr));
     %set_param('Kind/contract/input1', 'CopyFcn', testvar)
     for i = 1:length(blockTypes)
         if strcmp(blockTypes(i),'Inport') 
+            
+            %get the name of the inport block
+            inportBlockName = get_param(blockPaths(i), 'Name');
+            
             % get the inport outport
             input = get_param(blockPaths(i), 'PortHandles');
             inputLine = get_param(input{1,1}.Outport,'Line');
@@ -102,13 +128,14 @@ set_param(block,'MaskDisplay',char(portStr));
                 destinationBlockHandles = get_param(inputLine, 'DstBlockHandle');
             end
             
-            for j = 1 : (assumePorts + guaranteePorts + modeBlocksPorts)
+            for j = 1 : (assumePorts + guaranteePorts)
                 % if the line is not connected to the block
                 if ~ismember(portConnectivity(j).SrcBlock, destinationBlockHandles) 
                     
                     % disable the library links for the target block
                     % for the first time, the SrcBlock is -1, invalid.
                     set_param(portConnectivity(j).SrcBlock, 'LinkStatus', 'inactive');
+                    
                     % get the target block name
                     targetBlockName = get_param(portConnectivity(j).SrcBlock, 'Name');
                     % add new port inside that block
@@ -119,6 +146,54 @@ set_param(block,'MaskDisplay',char(portStr));
                     %connect the inport with the block            
                     add_line(blockModel, input{1,1}.Outport ,targetBlockPorts.Inport(length(targetBlockPorts.Inport)), 'autorouting','on');
                 end
+            end
+            
+            for j = (assumePorts + guaranteePorts +1) : (assumePorts + guaranteePorts+ modeBlocksPorts)
+                % disable the library links for the target block
+                % for the first time, the SrcBlock is -1, invalid.
+                set_param(portConnectivity(j).SrcBlock, 'LinkStatus', 'inactive');
+                modePorts =  get_param(portConnectivity(j).SrcBlock, 'PortHandles');
+                
+                % get the block of the require port
+                requireLine = get_param(modePorts.Inport(2), 'Line');
+                requireBlockHandle = get_param(requireLine, 'SrcBlockHandle');
+                 if ~ismember(requireBlockHandle, destinationBlockHandles) 
+                    
+                    % disable the library links for the target block
+                    % for the first time, the SrcBlock is -1, invalid.
+                    set_param(requireBlockHandle, 'LinkStatus', 'inactive');
+                    
+                    % get the target block name
+                    targetBlockName = get_param(requireBlockHandle, 'Name');
+                    % add new port inside that block
+                    add_block('built-in/Inport', ...
+                                    strcat(blockModel,'/',targetBlockName,'/','input'),'MakeNameUnique','on');
+                    
+                    targetBlockPorts = get_param(requireBlockHandle, 'PortHandles');
+                    %connect the inport with the block            
+                    add_line(blockModel, input{1,1}.Outport ,targetBlockPorts.Inport(length(targetBlockPorts.Inport)), 'autorouting','on');
+                 end            
+                
+                 % get the block of the ensure port
+                ensureLine = get_param(modePorts.Inport(3), 'Line');
+                ensureBlockHandle = get_param(ensureLine, 'SrcBlockHandle');
+                 if ~ismember(ensureBlockHandle, destinationBlockHandles) 
+                    
+                    % disable the library links for the target block
+                    % for the first time, the SrcBlock is -1, invalid.
+                    set_param(ensureBlockHandle, 'LinkStatus', 'inactive');
+                    
+                    % get the target block name
+                    targetBlockName = get_param(ensureBlockHandle, 'Name');
+                    % add new port inside that block
+                    add_block('built-in/Inport', ...
+                                    strcat(blockModel,'/',targetBlockName,'/','input'),'MakeNameUnique','on');
+                    
+                    targetBlockPorts = get_param(ensureBlockHandle, 'PortHandles');
+                    %connect the inport with the block            
+                    add_line(blockModel, input{1,1}.Outport ,targetBlockPorts.Inport(length(targetBlockPorts.Inport)), 'autorouting','on');
+                 end      
+                 
             end
         end
     end
